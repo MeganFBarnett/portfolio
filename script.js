ddadfaf1f7c25975bdc631e144900a66ee5af83e
EC2 Default User <ec2-user@ip-172-31-7-180.ec2.internal>
2020-09-01 19:00:35 +0000

    First Revison

---
+++ script.js
@@ -0,0 +1,58 @@
+
+//nav
+		function openNav() {
+			document.getElementById("mobileNav").style.height = "100%";
+		}
+
+		function closeNav() {
+			document.getElementById("mobileNav").style.height = "0%";
+		}
+
+//slide Darragh
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("darraghSlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+//slide class
+var slideIndex = 1;
+showDivs2(slideIndex);
+
+function plusDivs2(n) {
+  showDivs2(slideIndex += n);
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("classSlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+
+//Tab JS
+function openTab(tabName) {
+  var i;
+  var x = document.getElementsByClassName("tabContent");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
